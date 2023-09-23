1037d3098b3c0e721c2131d1420741294d0981e2
Tahir Asadli <asadovtahir@gmail.com>
2023-09-23 10:09:31 +0400

    Add media migration

---
+++ src/database/migrations/tacms_create_media_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('media', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('extension');
+            $table->string('mime');
+            $table->string('type')->nullable();
+            $table->string('size')->default('original');
+            $table->string('path');
+            $table->string('parent')->default(0);
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('media');
+    }
+};
