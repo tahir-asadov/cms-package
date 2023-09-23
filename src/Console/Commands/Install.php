75cf3adaf025977d7c3be90d9f6f270c426a3ef0
Tahir Asadli <asadovtahir@gmail.com>
2023-09-23 09:40:29 +0400

    Update install commands

--- src/Console/Commands/Install.php
+++ src/Console/Commands/Install.php
@@ -29,7 +29,17 @@ class Install extends Command
     public function handle()
     {
         $is_first_run = false;
-        $this->info('Publishing TACMS Assets...');
+
+        $web_route_file = base_path('routes/web.php');
+        $tacms_require_line = 'require __DIR__.\'/tacms.php\';';
+        $webRoutes = file_get_contents($web_route_file);
+        $has_the_line = Str::contains($webRoutes, $tacms_require_line);
+
+        if (!$has_the_line) {
+            $is_first_run = true;
+        }
+
+        $this->comment('Publishing TACMS Assets...');
         $this->comment('Publishing TACMS Configs...');
         $this->callSilent('vendor:publish', ['--tag' => 'tacms-config', '--force' => $this->option('force')]);
 
@@ -45,51 +55,37 @@ class Install extends Command
 
         $this->comment('Publishing TACMS Routes...');
         $this->callSilent('vendor:publish', ['--tag' => 'tacms-routes', '--force' => $this->option('force')]);
-
-        $this->info('TACMS scaffolding installed successfully.');
         
-        $this->comment('Add npm packages...');
-        Process::run('npm i @tahir-asadli/uploader');
-        Process::run('npm i slugify');
-        // $this->callSilent('npm i @tahir-asadli/uploader', ['--tag' => 'tacms-npm-uploader', '--force' => true]);
-        // $this->callSilent('npm i slugify', ['--tag' => 'tacms-npm-slugify', '--force' => true]);
         
-        $this->comment('Installing TACMS Routes...');
-
-        $this->comment('Installing Breeze...');
-        $this->call('breeze:install', ['stack' => 'blade']);
-        $this->info('Breeze installed successfully.');
+        if($is_first_run) {
+            $this->comment('Installing Breeze...');
+            $this->call('breeze:install', ['stack' => 'blade']);
+            $this->info('Breeze installed successfully.');
+        }
 
         $this->comment('Publishing Vite config...');
         $this->callSilent('vendor:publish', ['--tag' => 'tacms-vite', '--force' => true]);
 
+
+        
+        $this->comment('Installing npm packages...');
+        Process::run('npm i @tahir-asadli/uploader');
+        Process::run('npm i slugify');
+
         
 
-        $web_route_file = base_path('routes/web.php');
-        $tacms_require_line = 'require __DIR__.\'/tacms.php\';';
-        
-        $webRoutes = file_get_contents($web_route_file);
-        dump($webRoutes);
-        $has_the_line = Str::contains($webRoutes, $tacms_require_line);
-        if (!$has_the_line) {
-            $is_first_run = true;
+        if ($is_first_run) {
+            $this->comment('Adding TACMS Routes...');
             file_put_contents($web_route_file, "\r\n" . $tacms_require_line, FILE_APPEND);
         }
-        $webRoutes = file_get_contents($web_route_file);
-        dump($webRoutes);
-        if($has_the_line){
-            dump('has the line');
-        }else {
-            dump('doesn\'t have the line');
-        }
 
-        $this->info('TACMS Routes installed successfully.');
-        $this->info($is_first_run ? 'first run' : 'not first run');
         if($is_first_run){
             $this->comment('Running migrations...');
             $this->call('migrate');
-            $this->info('Database tables created successfully.');
+            $this->comment('Database tables created successfully.');
         }
+
+        $this->info('TACMS scaffolding installed successfully.');
         
         
     }
