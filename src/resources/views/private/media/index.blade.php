ed09528e7697f522033e103735a1ba1701cd3dd1
Tahir Asadli <asadovtahir@gmail.com>
2023-09-24 11:12:07 +0400

    Add media controller

---
+++ src/resources/views/private/media/index.blade.php
@@ -0,0 +1,4 @@
+@extends('tacms::layouts.private')
+@section('content')
+Media view: <code>{{ config('tacms.homepage') }}</code>
+@endsection
--- src/routes/tacms.php
+++ src/routes/tacms.php
@@ -1,9 +1,16 @@
 <?php
 
+use App\Http\Controllers\MediaController;
 use Illuminate\Support\Facades\Route;
 use Tahir\CMS\Controllers\PublicController;
 
 Route::controller(PublicController::class)->group(function () {
+  Route::get('', 'index');
+});
+
+
+
+Route::controller(MediaController::class)->group(function () {
   Route::get('', 'index');
 });
 
