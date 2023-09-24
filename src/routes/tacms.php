b2c0b9f4d47432e3d0d1f01015363776ac971051
Tahir Asadli <asadovtahir@gmail.com>
2023-09-24 11:54:24 +0400

    Fix bug

--- src/routes/tacms.php
+++ src/routes/tacms.php
@@ -1,8 +1,8 @@
 <?php
 
-use Tahir\CMS\Controllers\MediaController;
+use Tahir\CMS\Http\Controllers\MediaController;
 use Illuminate\Support\Facades\Route;
-use Tahir\CMS\Controllers\PublicController;
+use Tahir\CMS\Http\Controllers\PublicController;
 
 Route::controller(PublicController::class)->group(function () {
   Route::get('', 'index');
